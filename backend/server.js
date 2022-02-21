28a0b319eff1e0a3a024efe838a644656082d52b
surajsharma <surajsharma@pm.me>
2022-02-21 13:14:00 +0530

    several fixes for server/config

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,12 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-// app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-// app.get("*", function (request, response) {
-//     response.sendFile(
-//         path.resolve(__dirname, "../frontend/build", "index.html")
-//     );
-// });
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+app.get("*", function (request, response) {
+    response.sendFile(
+        path.resolve(__dirname, "../frontend/build", "index.html")
+    );
+});
 
 app.use(cors({ credentials: true }));
 app.use(express.json());
